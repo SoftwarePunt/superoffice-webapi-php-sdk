6ec779449e9730c3692e395079ffe8fcc37b6ad9
Roy de Jong <email@roydejong.net>
2020-05-22 13:28:08 +0200

    ProjectCollection: Add getById($id)

--- src/Collections/Projects/ProjectCollection.php
+++ src/Collections/Projects/ProjectCollection.php
@@ -4,15 +4,43 @@ namespace roydejong\SoWebApi\Collections\Projects;
 
 use roydejong\SoWebApi\Collections\Collection;
 use roydejong\SoWebApi\Structs\Projects\ProjectEntity;
+use roydejong\SoWebApi\WebApiException;
 
 class ProjectCollection extends Collection
 {
     public static string $PATH = "/api/v1/Project";
 
+    /**
+     * Gets a stub project entity with default values.
+     *
+     * Set default values into a new ProjectEntity.
+     * NetServer calculates default values on the entity, which is required when creating/storing a new instance.
+     *
+     * @see https://community.superoffice.com/documentation/sdk/SO.NetServer.Web.Services/html/v1ProjectEntity_DefaultProjectEntity.htm
+     *
+     * @return ProjectEntity
+     * @throws WebApiException
+     */
     public function getDefault(): ProjectEntity
     {
         return ProjectEntity::fromResponse(
             $this->client->get(self::$PATH . "/default")
+        );
+    }
+
+    /**
+     * Gets a ProjectEntity object by id.
+     *
+     * @see https://community.superoffice.com/documentation/sdk/SO.NetServer.Web.Services/html/v1ProjectEntity_GetProjectEntity.htm
+     *
+     * @param int $id The id of the ProjectEntity to return.
+     * @return ProjectEntity
+     * @throws WebApiException
+     */
+    public function getById(int $id): ProjectEntity
+    {
+        return ProjectEntity::fromResponse(
+            $this->client->get(self::$PATH . "/{$id}")
         );
     }
 }
--- tests/Collections/Projects/ProjectCollectionTest.php
+++ tests/Collections/Projects/ProjectCollectionTest.php
@@ -11,6 +11,9 @@ class ProjectCollectionTest extends TestCase
 {
     public function testGetDefault()
     {
+        /**
+         * @var $request RequestInterface
+         */
         $request = null;
 
         $client = new MockClient();
@@ -24,11 +27,41 @@ class ProjectCollectionTest extends TestCase
         $collection = $client->projects();
         $defaultProject = $collection->getDefault();
 
+        $this->assertSame("https://mock.superoffice.com/Cust12345/api/v1/Project/default", (string)$request->getUri());
+
         $this->assertInstanceOf("roydejong\SoWebApi\Structs\Projects\ProjectEntity", $defaultProject,
             "Calling products()->getDefault() should return a single project entity");
 
         $this->assertSame(0, $defaultProject->ProjectId);
         $this->assertSame("10015", $defaultProject->ProjectNumber);
         $this->assertSame("Conference", $defaultProject->ProjectType->Value);
+    }
+
+    public function testGetById()
+    {
+        /**
+         * @var $request RequestInterface
+         */
+        $request = null;
+
+        $client = new MockClient();
+        $client->setMockHandler(function (RequestInterface $_request) use (&$request) {
+            $request = $_request;
+
+            return new Response(200, [],
+                file_get_contents(__DIR__ . "/../../_samples/project_1_response.json"));
+        });
+
+        $collection = $client->projects();
+        $defaultProject = $collection->getById(1);
+
+        $this->assertSame("https://mock.superoffice.com/Cust12345/api/v1/Project/1", (string)$request->getUri());
+
+        $this->assertInstanceOf("roydejong\SoWebApi\Structs\Projects\ProjectEntity", $defaultProject,
+            "Calling products()->getDefault() should return a single project entity");
+
+        $this->assertSame(1, $defaultProject->ProjectId);
+        $this->assertSame("Example: Internal project", $defaultProject->Name);
+        $this->assertSame("10011", $defaultProject->ProjectNumber);
     }
 }
