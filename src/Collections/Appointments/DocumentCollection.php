49812ae5edee44cd0343daaad7cdcf11204b0e28
Roy de Jong <email@roydejong.net>
2020-06-02 19:39:02 +0200

    Documents: Add getContentStringById()

--- src/Collections/Appointments/DocumentCollection.php
+++ src/Collections/Appointments/DocumentCollection.php
@@ -7,4 +7,19 @@ use roydejong\SoWebApi\Collections\Collection;
 class DocumentCollection extends Collection
 {
     public static string $PATH = "/api/v1/Document";
+
+    /**
+     * Streams the document content into a string value.
+     *
+     * @param int $documentId
+     * @return string
+     */
+    public function getContentStringById(int $documentId): string
+    {
+        return $this->client->get(self::$PATH . "/{$documentId}/Content", [
+            'headers' => [
+                'Accept' => "*"
+            ]
+        ])->getBody();
+    }
 }
--- tests/Collections/Appointments/DocumentCollectionTest.php
+++ tests/Collections/Appointments/DocumentCollectionTest.php
@@ -31,4 +31,26 @@ class DocumentCollectionTest extends TestCase
         $this->assertCount(2, $allDocuments, 'Query should return one result for sample response');
         $this->assertInstanceOf("roydejong\SoWebApi\Structs\Appointments\ODataDocumentItem", $allDocuments[0]);
     }
+
+    public function testGetContentStringById()
+    {
+        /**
+         * @var $request RequestInterface
+         */
+        $request = null;
+
+        $client = new MockClient();
+        $client->setMockHandler(function (RequestInterface $_request) use (&$request) {
+            $request = $_request;
+
+            return new Response(200, [], "oh hi");
+        });
+
+        $collection = $client->documents();
+        $raw = $collection->getContentStringById(123);
+
+        $this->assertSame("oh hi", $raw);
+        $this->assertStringEndsWith("/api/v1/Document/123/Content", (string)$request->getUri());
+        $this->assertSame("*", $request->getHeader('Accept')[0]);
+    }
 }
